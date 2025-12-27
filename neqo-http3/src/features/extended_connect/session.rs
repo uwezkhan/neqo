406be9c9226d7763a64740bcd7f48867875c501f
Randell Jesup <rjesup@mozilla.com>
2025-12-27 17:04:37 -0500

    add interfaces to set anticipated incoming uni/bi-directional streams

--- neqo-http3/src/features/extended_connect/session.rs
+++ neqo-http3/src/features/extended_connect/session.rs
@@ -62,6 +62,10 @@ pub(crate) struct Session {
     /// CONNECT request.
     protocol: Box<dyn Protocol>,
     draining: bool,
+    /// Anticipated concurrent incoming unidirectional streams for this session.
+    anticipated_incoming_uni: u16,
+    /// Anticipated concurrent incoming bidirectional streams for this session.
+    anticipated_incoming_bidi: u16,
 }
 
 #[derive(Debug, PartialEq, Clone, Copy)]
@@ -122,6 +126,8 @@ impl Session {
             events,
             protocol,
             draining: false,
+            anticipated_incoming_uni: 0,
+            anticipated_incoming_bidi: 0,
         }
     }
 
@@ -152,6 +158,8 @@ impl Session {
             events,
             protocol,
             draining: false,
+            anticipated_incoming_uni: 0,
+            anticipated_incoming_bidi: 0,
         })
     }
 
@@ -522,6 +530,22 @@ impl Session {
         now: Instant,
     ) -> Vec<super::datagram_queue::DatagramOutcome> {
         self.protocol.set_datagram_max_age(age_ms, now)
+    }
+
+    pub(crate) const fn set_anticipated_incoming_uni(&mut self, value: u16) {
+        self.anticipated_incoming_uni = value;
+    }
+
+    pub(crate) const fn anticipated_incoming_uni(&self) -> u16 {
+        self.anticipated_incoming_uni
+    }
+
+    pub(crate) const fn set_anticipated_incoming_bidi(&mut self, value: u16) {
+        self.anticipated_incoming_bidi = value;
+    }
+
+    pub(crate) const fn anticipated_incoming_bidi(&self) -> u16 {
+        self.anticipated_incoming_bidi
     }
 
     pub(crate) fn datagram(&mut self, datagram: Bytes) {
